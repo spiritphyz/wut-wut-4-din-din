359177228076a219d56087e9e0a0e2836ba0ae8b
spiritphyz <tonyle@ocf.berkeley.edu>
2016-11-21 16:12:08 -0800

    Implement Redis to save on API call tokens

--- data/db-helpers.js
+++ data/db-helpers.js
@@ -1,9 +1,12 @@
 require('../server/api-keys.js');
 const request = require('request');
 const bodyParser = require('body-parser');
+const redis = require('redis');
 const mashapeKey = process.env.MASHAPE_KEY;
 
-exports.gatherRecipes = function(req, res) {
+const redisClient = redis.createClient();
+
+exports.gatherRecipes = function(sortedItems, res) {
 
   // NOTE: live API request is disabled because the
   // Spoonacular recipes endpoint seems to only give me 50 free
@@ -12,73 +15,79 @@ exports.gatherRecipes = function(req, res) {
 
   // note: req.query (not req.body) is for basic HTML forms it seems
   // const items = req.query.seasonalItems;
-  // const options = {
-  //   url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients='
-  //     + items
-  //     + '&limitLicense=false&number=5&ranking=1',
-  //   headers: {
-  //     'X-Mashape-Key': mashapeKey,
-  //     'Accept': 'application/json'
-  //   }
-  // };
-   
-  // const cb = (err, response, body) => {
-  //   if (!err) {
-  //     const data = JSON.parse(body);
-  //     res.status(200).send(data);
-  //   }
-  // };
 
-  // request(options, cb);
+  const items = sortedItems;
+  const options = {
+    url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients='
+      + items
+      + '&limitLicense=false&number=5&ranking=1',
+    headers: {
+      'X-Mashape-Key': mashapeKey,
+      'Accept': 'application/json'
+    }
+  };
+   
+  const cb = (err, response, body) => {
+    if (err) {
+      console.log('🍊  error in Spoonacular API lookup!', err);
+      res.status(404).send('Error in Spoonacular API lookup');
+    } else {
+      const parsedData = JSON.parse(body);
+      redisClient.set(items, body, redis.print);
+      res.status(200).send(parsedData);
+    }
+  };
+
+  request(options, cb);
 
   /* Enable this to send back dummy data to avoid getting charged
      for expensive Spoonacular API calls */
-  const data = [
-    {
-      'id': 625596,
-      'title': 'Roasted Brussels Sprouts, Cauliflower, and Radishes With Garlic Aioli From The \'Kitchn Cookbook\'',
-      'image': 'https://spoonacular.com/recipeImages/Roasted-Brussels-Sprouts--Cauliflower--and-Radishes-With-Garlic-Aioli-From-The-Kitchn-Cookbook-625596.jpg',
-      'imageType': 'jpg',
-      'usedIngredientCount': 2,
-      'missedIngredientCount': 2,
-      'likes': 11
-    },
-    {
-      'id': 450729,
-      'title': 'Roasted Cauliflower & Brussels Sprouts with Bacon',
-      'image': 'https://spoonacular.com/recipeImages/Roasted-Cauliflower---Brussels-Sprouts-with-Bacon-450729.jpg',
-      'imageType': 'jpg',
-      'usedIngredientCount': 2,
-      'missedIngredientCount': 2,
-      'likes': 0
-    },
-    {
-      'id': 762045,
-      'title': 'Roasted Cauliflower and Brussels Sprouts',
-      'image': 'https://spoonacular.com/recipeImages/roasted-cauliflower-and-brussels-sprouts-762045.jpg',
-      'imageType': 'jpg',
-      'usedIngredientCount': 2,
-      'missedIngredientCount': 2,
-      'likes': 632
-    },
-    {
-      'id': 11016,
-      'title': 'Herbed Chicken Cutlets With Roasted Winter Vegetables',
-      'image': 'https://spoonacular.com/recipeImages/herbed_chicken_cutlets_with_roasted_winter_vegetables-11016.jpg',
-      'imageType': 'jpg',
-      'usedIngredientCount': 2,
-      'missedIngredientCount': 2,
-      'likes': 67
-    },
-    {
-      'id': 23650,
-      'title': 'Cauliflower, Fried Aubergine And Black Olives',
-      'image': 'https://spoonacular.com/recipeImages/cauliflower-fried-aubergine-and-black-olives-2-23650.jpg',
-      'imageType': 'jpg',
-      'usedIngredientCount': 2,
-      'missedIngredientCount': 3,
-      'likes': 2
-    }
-  ];
-  res.status(200).send(data);
+  // const data = [
+  //   {
+  //     'id': 625596,
+  //     'title': 'Roasted Brussels Sprouts, Cauliflower, and Radishes With Garlic Aioli From The \'Kitchn Cookbook\'',
+  //     'image': 'https://spoonacular.com/recipeImages/Roasted-Brussels-Sprouts--Cauliflower--and-Radishes-With-Garlic-Aioli-From-The-Kitchn-Cookbook-625596.jpg',
+  //     'imageType': 'jpg',
+  //     'usedIngredientCount': 2,
+  //     'missedIngredientCount': 2,
+  //     'likes': 11
+  //   },
+  //   {
+  //     'id': 450729,
+  //     'title': 'Roasted Cauliflower & Brussels Sprouts with Bacon',
+  //     'image': 'https://spoonacular.com/recipeImages/Roasted-Cauliflower---Brussels-Sprouts-with-Bacon-450729.jpg',
+  //     'imageType': 'jpg',
+  //     'usedIngredientCount': 2,
+  //     'missedIngredientCount': 2,
+  //     'likes': 0
+  //   },
+  //   {
+  //     'id': 762045,
+  //     'title': 'Roasted Cauliflower and Brussels Sprouts',
+  //     'image': 'https://spoonacular.com/recipeImages/roasted-cauliflower-and-brussels-sprouts-762045.jpg',
+  //     'imageType': 'jpg',
+  //     'usedIngredientCount': 2,
+  //     'missedIngredientCount': 2,
+  //     'likes': 632
+  //   },
+  //   {
+  //     'id': 11016,
+  //     'title': 'Herbed Chicken Cutlets With Roasted Winter Vegetables',
+  //     'image': 'https://spoonacular.com/recipeImages/herbed_chicken_cutlets_with_roasted_winter_vegetables-11016.jpg',
+  //     'imageType': 'jpg',
+  //     'usedIngredientCount': 2,
+  //     'missedIngredientCount': 2,
+  //     'likes': 67
+  //   },
+  //   {
+  //     'id': 23650,
+  //     'title': 'Cauliflower, Fried Aubergine And Black Olives',
+  //     'image': 'https://spoonacular.com/recipeImages/cauliflower-fried-aubergine-and-black-olives-2-23650.jpg',
+  //     'imageType': 'jpg',
+  //     'usedIngredientCount': 2,
+  //     'missedIngredientCount': 3,
+  //     'likes': 2
+  //   }
+  // ];
+  // res.status(200).send(data);
 };
--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('../data/db-helpers.js');
+const metadata = require('../package.json');
 const port = '1688';
+const redis = require('redis');
 require('./api-keys.js');
 
 // attach services
 const app = express();
+const redisClient = redis.createClient();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -18,8 +21,26 @@ app.get('/', function(req, res) {
 });
 
 // getRecipe route
+/* Input: req.query is an object of key-value pairs */
 app.get('/getRecipes', function(req, res) {
-  db.gatherRecipes(req, res);
+  const query = req.query.seasonalItems;
+  const sortedItems = query.split(',')
+    .sort((x, y) => x.localeCompare(y))
+    .join(',');
+  redisClient.get(sortedItems, function(err, reply) {
+    if (err) {
+      console.log('🍊  Error in fetching from Redis', err);
+      res.status(200).send('Error in fetching from Redis', err);
+    }
+    if (reply) {
+      var parsed = JSON.parse(reply);
+      console.log('🍊  Found in Redis!', sortedItems);
+      res.status(200).send(parsed);
+    } else {
+      console.log('🍊  Not found in Redis:', query);
+      db.gatherRecipes(sortedItems, res);
+    }
+  });
 });
 
 // wildcard route
@@ -29,4 +50,4 @@ app.get('*', function(req, res) {
 
 // start server
 app.listen(port);
-console.log('🍊  listening on', port);
+console.log('🍊  listening on', port, 'for app:', metadata.name);
