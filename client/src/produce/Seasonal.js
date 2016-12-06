dcf28444d9784846bcbadd3fb2d30481b5f27b4c
spiritphyz <tonyle@ocf.berkeley.edu>
2016-12-06 06:28:25 -0800

    Fix spelling, bad data in December produce

--- client/src/produce/Seasonal.js
+++ client/src/produce/Seasonal.js
@@ -39,7 +39,7 @@ export const Seasonal = {
     },
     'November': {
       'early': {
-        'produce': [ 'Apples', 'Asparagus', 'Avocados', 'Beets', 'Bok Choy', 'Broccoli', 'Brussels Sprouts', 'Cabbage', 'Carrots', 'Cauliflower', 'Celery', 'Cucumbers', 'Eggplant', 'Fennel', 'Grapefruit', 'Grapes', 'Halibut', 'Pacific', 'Kale', 'Lettuce', 'Mushrooms', 'Okra', 'Onions', 'Oranges', 'Peaches', 'Pears', 'Peas', 'Pistachios', 'Potatoes', 'Radishes', 'Raspberries', 'Rhubarb', 'Rutabaga', 'Scallions', 'Shrimp - Pink', 'Snap Peas', 'Spinach', 'Squash', 'Sweet Potatoes', 'Tomatoes', 'Turkey - Bourbon Red', 'Turkey - Standard Bronze', 'Turnips']
+        'produce': [ 'Apples', 'Asparagus', 'Avocados', 'Beets', 'Bok Choy', 'Broccoli', 'Brussels Sprouts', 'Cabbage', 'Carrots', 'Cauliflower', 'Celery', 'Cucumbers', 'Eggplant', 'Fennel', 'Grapefruit', 'Grapes', 'Kale', 'Lettuce', 'Mushrooms', 'Okra', 'Onions', 'Oranges', 'Pacific Halibut', 'Peaches', 'Pears', 'Peas', 'Pistachios', 'Potatoes', 'Radishes', 'Raspberries', 'Rhubarb', 'Rutabaga', 'Scallions', 'Shrimp - Pink', 'Snap Peas', 'Spinach', 'Squash', 'Sweet Potatoes', 'Tomatoes', 'Turkey - Bourbon Red', 'Turkey - Standard Bronze', 'Turnips']
       },
       'late': {
         'produce': [ 'Apples', 'Asparagus', 'Avocados', 'Beets', 'Bok Choy', 'Broccoli', 'Brussels Sprouts', 'Cabbage', 'Carrots', 'Cauliflower', 'Celery', 'Cucumbers', 'Eggplant', 'Fennel', 'Grapefruit', 'Grapes', 'Halibut', 'Pacific', 'Kale', 'Lettuce', 'Mushrooms', 'Okra', 'Onions', 'Oranges', 'Peaches', 'Pears', 'Peas', 'Pistachios', 'Potatoes', 'Radishes', 'Raspberries', 'Rhubarb', 'Rutabaga', 'Scallions', 'Shrimp - Pink', 'Snap Peas', 'Spinach', 'Squash', 'Sweet Potatoes', 'Tomatoes', 'Turkey - Bourbon Red', 'Turkey - Standard Bronze', 'Turnips']
@@ -47,10 +47,10 @@ export const Seasonal = {
     },
     'December': {
       'early': {
-        'produce': [ 'Apples', 'Avocados', 'Beets', 'Bok Choy', 'Broccoli', 'Brussels Sprouts', 'Cabbage', 'Carrots', 'Cauliflower', 'Celery', 'Cucumbers', 'Fennel', 'Grapefruit', 'Grapes', 'Halibut', 'Pacific', 'Kale', 'Lettuce', 'Mushrooms', 'Okra', 'Onions', 'Oranges', 'Pears', 'Pistachios', 'Potatoes', 'Radishes', 'Rutabaga', 'Scallions', 'Shrimp', 'Pink', 'Snap Peas', 'Spinach', 'Squash', 'Sweet Potatoes', 'Tomatoes', 'Turkey - Bourbon Red', 'Turkey - Standard Bronze', 'Turnips'] }, 'late': { 'produce': [ 'Apples', 'Avocados', 'Beets', 'Bok Choy', 'Broccoli', 'Brussels Sprouts', 'Cabbage', 'Carrots', 'Cauliflower', 'Celery', 'Cucumbers', 'Fennel', 'Grapefruit', 'Grapes', 'Halibut', 'Pacific', 'Kale', 'Lettuce', 'Mushrooms', 'Okra', 'Onions', 'Oranges', 'Pears', 'Pistachios', 'Potatoes', 'Radishes', 'Rutabaga', 'Scallions', 'Shrimp', 'Pink', 'Snap Peas', 'Spinach', 'Squash', 'Sweet Potatoes', 'Tomatoes', 'Turkey - Bourbon Red', 'Turkey - Standard Bronze', 'Turnips']
+        'produce': [ 'Apples', 'Avocados', 'Beets', 'Bok Choy', 'Broccoli', 'Brussels Sprouts', 'Cabbage', 'Carrots', 'Cauliflower', 'Celery', 'Cucumbers', 'Fennel', 'Grapefruit', 'Grapes', 'Kale', 'Lettuce', 'Mushrooms', 'Okra', 'Onions', 'Oranges', 'Pacific Halibut', 'Pears', 'Pink Shrimp', 'Pistachios', 'Potatoes', 'Radishes', 'Rutabaga', 'Scallions', 'Snap Peas', 'Spinach', 'Squash', 'Sweet Potatoes', 'Tomatoes', 'Turkey - Bourbon Red', 'Turkey - Standard Bronze', 'Turnips']
       },
       'late': {
-        'produce': [ 'Apples', 'Avocados', 'Beets', 'Bok Choy', 'Broccoli', 'Brussels Sprouts', 'Cabbage', 'Carrots', 'Cauliflower', 'Celery', 'Cucumbers', 'Fennel', 'Grapefruit', 'Grapes', 'Halibut', 'Pacific', 'Kale', 'Lettuce', 'Mushrooms', 'Okra', 'Onions', 'Oranges', 'Pears', 'Pistachios', 'Potatoes', 'Radishes', 'Rutabaga', 'Scallions', 'Shrimp', 'Pink', 'Snap Peas', 'Spinach', 'Squash', 'Sweet Potatoes', 'Tomatoes', 'Turkey - Bourbon Red', 'Turkey - Standard Bronze', 'Turnips']
+        'produce': [ 'Apples', 'Avocados', 'Beets', 'Bok Choy', 'Broccoli', 'Brussels Sprouts', 'Cabbage', 'Carrots', 'Cauliflower', 'Celery', 'Cucumbers', 'Fennel', 'Grapefruit', 'Grapes', 'Pacific Halibut', 'Kale', 'Lettuce', 'Mushrooms', 'Okra', 'Onions', 'Oranges', 'Pears', 'Pistachios', 'Pink Shrimp', 'Potatoes', 'Radishes', 'Rutabaga', 'Scallions', 'Snap Peas', 'Spinach', 'Squash', 'Sweet Potatoes', 'Tomatoes', 'Turkey - Bourbon Red', 'Turkey - Standard Bronze', 'Turnips']
       }
     }
   }
