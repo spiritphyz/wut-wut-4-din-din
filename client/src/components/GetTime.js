addc4b9fb0c8db6015d13dfc9847f1bc9fc61f87
spiritphyz <tonyle@ocf.berkeley.edu>
2016-12-10 16:41:02 -0800

    Remove reliance on MomentJS for dates

--- client/src/components/GetTime.js
+++ client/src/components/GetTime.js
@@ -1,12 +1,27 @@
 export const GetTime = request => {
+  const months = {
+    '1': 'January',
+    '2': 'February',
+    '3': 'March',
+    '4': 'April',
+    '5': 'May',
+    '6': 'June',
+    '7': 'July',
+    '8': 'August',
+    '9': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December'
+  };
+  const date = new Date();
   if (request === 'day') {
-    const day = moment().format('D');
-    // console.log('🍊  Requested day is', day);
+    const day = date.getDate();
+    console.log('🍊  Requested day is', day);
     return day;
   }
   if (request === 'month') {
-    const month = moment().format('MMMM');
-    // console.log('🍊  Requested month is', month);
+    const month = months[ date.getMonth() + 1 ];
+    console.log('🍊  Requested month is', month);
     return month;
   }
 };
