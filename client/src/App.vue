d6c89436905607fb3c11d92729167537a5bee028
spiritphyz <tonyle@ocf.berkeley.edu>
2017-02-16 11:03:09 -0800

    Adjust positioning for mobile clients

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="container">
     <div class="eggplant">
-      <img src="https://cloud.githubusercontent.com/assets/7908723/20235152/59d8d64a-a83f-11e6-9534-4119d03f3874.png" width="100" align="left">
+      <img src="https://cloud.githubusercontent.com/assets/7908723/20235152/59d8d64a-a83f-11e6-9534-4119d03f3874.png" width="80" align="left">
     </div>
     <br><br><br><br>
     <h1>What&rsquo;s For Dinner?</h1>
@@ -36,16 +36,18 @@
   }
 
   .eggplant {
-    padding: 10px 20px 70px 20px;
+    padding: 10px 20px 40px 20px;
   }
 
   h1 {
+    font-size: 26px;
     font-weight: regular;
     text-align: left;
     padding-left: 20px;
   }
 
   h2, h3, h4 {
+    font-size: 18px;
     font-weight: light;
     text-align: left;
     padding-left: 20px;
